6f853b95d3c6ed1e4e58bb0626ceb380d113e395
Rodney Cullen <rodney@stectech.com>
2017-03-26 23:25:43 +0700

    Added TA widget to show ratings

--- review.php
+++ review.php
@@ -1,33 +1,43 @@
-<?php include("template/header.php"); ?>
+<?php include("template/header.php");?>
 
     <!-- .container is main centered wrapper -->
     <div class="container">
-      <h3>We did it, COOL!</h3>
+      <h3>COOL, you like us!</h3>
         <div class="row">
           <div class="six columns" style="padding-bottom:20px;">
-            <p>You know what's even <strong>cooler?</strong> </br>Leaving reviews:</p>
+            <p>You know what's even <strong>cooler?</strong> Leaving reviews.</p>
+            <p>We're a small family business and our reviews are our lifeline. If you are enjoying your visit today I would be eternally grateful if you help spread the word. It should take less than 60 seconds and in exchange you will receive my eternal gratitude #staycool</p>
             <?php if (preg_match('/iPhone|iPad|iPod/i', $_SERVER['HTTP_USER_AGENT'])) {
-                echo '<a href="http://tripadvisor.com/Restaurant_Review-g293917-d7061335-Reviews-Overstand-Chiang_Mai.html">Link</a>';
-            } else {
-              echo '
+              echo '</br><a class="button button-primary" href="https://www.tripadvisor.com/UserReviewEdit-g293917-d7061335-e__2F__Restaurant__5F__Review__2D__g293917__2D__d7061335__2D__Reviews__2D__Overstand__2D__Chiang__5F__Mai__2E__html-Overstand-Chiang_Mai.html">Review on TripAdvisor</a>';
+
+} else {
+    echo '
               <div id="TA_cdswritereviewlg800" class="TA_cdswritereviewlg">
               <ul id="4xgF7IwYM" class="TA_links qmqGNsr">
               <li id="7Feu42p" class="meCI0wOSrwx">
-              <a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/medium-logo-12097-2.png" alt="TripAdvisor" /></a>
               </li>
               </ul>
               </div>';
-            }?>
+}?>
+<div id="TA_cdsratingsonlynarrow666" class="TA_cdsratingsonlynarrow">
+      <ul id="CQv9QK20u8b" class="TA_links MgYqjPZga5">
+      <li id="hBqCV9" class="zC2llq0">
+      <a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/tripadvisor_logo_transp_340x80-18034-2.png" alt="TripAdvisor"/></a>
+      </li>
+      </ul>
+      </div>
+      <script src="https://www.jscache.com/wejs?wtype=cdsratingsonlynarrow&amp;uniq=666&amp;locationId=7061335&amp;lang=en_US&amp;border=false&amp;display_version=2"></script>
+
 
 
 
           </div>
 
           <div class="one-half column">
-            <p>Don't have an account on TripAdvisor? No probs fam, We know you have one of these:</p>
-              <a class="button button-primary" href="https://search.google.com/local/writereview?placeid=ChIJjz1yj6E62jARRzni4mW0N_M">Google</a>
+            <p>Don't have an account on TripAdvisor? No probs fam, we got you covered:</p>
+              <a class="button button-primary" href="https://search.google.com/local/writereview?placeid=ChIJjz1yj6E62jARRzni4mW0N_M">Review on Google</a>
               <!-- <a class="button button-primary" href="https://www.tripadvisor.com/UserReviewEdit-g293917-d7061335-a_referredFromLocationSearch.true-a_ReviewName.-a_type.-e-wpage1-Overstand-Chiang_Mai.html">TripAdvisor</a> -->
-              <a class="button button-primary" href="https://www.facebook.com/pg/OverstandCoffee/reviews/">Facebook</a>
+              <a class="button button-primary" href="https://www.facebook.com/pg/OverstandCoffee/reviews/">Review on Facebook</a>
           </div>
         </div>
     </div>
