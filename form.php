ed5cf7e3649c831c275f1cc88d70a6daca5b0fc7
Rodney Cullen <rodcul@gmail.com>
2017-04-04 09:10:07 +0700

    Saving ratings to db

--- form.php
+++ form.php
@@ -1,17 +1,19 @@
 <?php
 include('config.php');
-
-
-
 // $feedback = $_POST['feedback'];
 
 $rating = isset($_POST['rating']) ? $_POST['rating'] : '';
 $feedback = isset($_POST['feedback']) ? $_POST['feedback'] : '';
-
+$uid = isset($_COOKIE['wifi']['uid']) ? $_COOKIE['wifi']['uid'] : '';
 
 $email_subject = "WIFI: $rating $feedback";
 $email_body = "Put something here";
 $email_from = isset($_COOKIE['wifi']['email']) ? $_COOKIE['wifi']['email'] : 'rodney@overstand.co.th';
+
+$sql = 'INSERT INTO ratings '.
+   '(uid,rating,email) '.
+   "VALUES ( '$uid', '$rating', '$email_from' )";
+
 
 $headers = "From: $email_from \r\n";
 // $headers .= "Reply-To: $visitor_email \r\n";
@@ -20,9 +22,11 @@ mail($email_to, $email_subject, $email_body);
 if (empty($feedback)) {
     if ($rating >= 4) {
         header('Location: review.php');
+        include('template/mysql.php');
         exit();
     } else {
         header('Location: feedback.php');
+        include('template/mysql.php');
         exit();
     }
 } else {
---
+++ template/mysql.php
@@ -0,0 +1,21 @@
+<?php
+   include('../config.php');
+
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+
+
+   mysql_select_db('hotspot');
+   $retval = mysql_query( $sql, $conn );
+
+   if(! $retval ) {
+      die('Could not enter data: ' . mysql_error());
+   }
+
+   echo "Entered data successfully\n";
+
+   mysql_close($conn);
+?>
