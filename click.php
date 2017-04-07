3f811f60f6cb5ade385eb2e272cfdcae0d43252b
Rodney Cullen <rodcul@gmail.com>
2017-04-07 13:00:04 +0700

    Data now comes from PHP, can change timezone in config

--- click.php
+++ click.php
@@ -2,8 +2,10 @@
   include 'config.php';
   $uid = isset($_COOKIE['wifi']['uid']) ? $_COOKIE['wifi']['uid'] : '';
   $url = $urls[$_GET['url']];
+  $create_time = date("Y-m-d H:i:s");
+
   header( "Location:" . $urls[$_GET['url']] );
-  $sql = 'INSERT INTO clicks '.'(uid,url) '. "VALUES ( '$uid', '{$_GET['url']}' )";
+  $sql = 'INSERT INTO clicks '.'(create_time, uid,url) '. "VALUES ( '$create_time', '$uid', '{$_GET['url']}' )";
   include('template/mysql.php');
   exit();
 ?>
--- form.php
+++ form.php
@@ -9,10 +9,11 @@ $uid = isset($_COOKIE['wifi']['uid']) ? $_COOKIE['wifi']['uid'] : '';
 $email_subject = "WIFI: $rating $feedback";
 $email_body = "Put something here";
 $email_from = isset($_COOKIE['wifi']['email']) ? $_COOKIE['wifi']['email'] : 'rodney@overstand.co.th';
+$create_time = date("Y-m-d H:i:s");
 
 $sql = 'INSERT INTO ratings '.
-   '(uid,rating,email) '.
-   "VALUES ( '$uid', '$rating', '$email_from' )";
+   '(create_time,uid,rating,email) '.
+   "VALUES ('$create_time', '$uid', '$rating', '$email_from' )";
 
 
 $headers = "From: $email_from \r\n";
